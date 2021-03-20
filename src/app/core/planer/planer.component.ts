59f248c2768140a63690897a3134549f5fb782ec
Kamil Rękas <zekse00@gmail.com>
2021-03-20 20:18:07 +0100

    add base structure navigation

---
+++ src/app/core/planer/planer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planer',
+  templateUrl: './planer.component.html',
+  styleUrls: ['./planer.component.css']
+})
+export class PlanerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
