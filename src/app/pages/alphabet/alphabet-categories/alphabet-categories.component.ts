557bb37701dbd12abb60b75ef914c41a7bf5525d
Adsel <marcin.radwan08@gmail.com>
2021-03-30 14:06:04 +0200

    feat: new module for exercise alphabet, new component for exercises categories

---
+++ src/app/pages/alphabet/alphabet-categories/alphabet-categories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alphabet-categories',
+  templateUrl: './alphabet-categories.component.html',
+  styleUrls: ['./alphabet-categories.component.css']
+})
+export class AlphabetCategoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/alphabet/alphabet.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlphabetCategoriesComponent } from './alphabet-categories/alphabet-categories.component';
+
+
+
+@NgModule({
+  declarations: [AlphabetCategoriesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AlphabetModule { }
